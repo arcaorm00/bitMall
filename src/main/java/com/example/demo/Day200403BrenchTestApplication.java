55d525b25e0eb6e58057f4f76095f8b08798b13d
arcaorm00 <arcaorm00@gmail.com>
2020-04-03 12:03:25 +0900

    brenchTest

---
+++ src/main/java/com/example/demo/Day200403BrenchTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day200403BrenchTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day200403BrenchTestApplication.class, args);
+	}
+
+}
